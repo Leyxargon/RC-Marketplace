3fe5d973c38df383b84b3a83c7cf4a5ade6d89f9
Leyxargon <43447240+Leyxargon@users.noreply.github.com>
2020-01-23 18:37:19 +0100

    Update serverM.c

--- serverM.c
+++ serverM.c
@@ -1,12 +1,3 @@
-/**
- * Semplice programma che invia ai client il Daytime del server.
- *
- * Il programma è abilitato alla gestione di più richieste in parallelo,
- * mediante la tecnica della programmazione concorrente. Il processo genitore
- * si occupa della gestione delle richieste (socket di richieste), mentre i
- * processi figli si occupano del completamento delle richieste (socket accettati).
- * @author Dario Musella
- */
 #include "liste/lista.h"
 #include "wrapper.h"
 #include "liste/iofile.h"
