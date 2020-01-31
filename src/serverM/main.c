236000cbc3546a98321006617410e657290a0676
Leyxargon <43447240+Leyxargon@users.noreply.github.com>
2020-01-31 22:59:11 +0100

    Pulizia del codice

--- src/serverM/main.c
+++ src/serverM/main.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
 	else
 		fputs("Dati caricati correttamente.\n", stdout);
 	fclose(dbf);
-    visualizzaDati(listaUtenti);
+	visualizzaDati(listaUtenti);
 	/************/
 
 	/* CONNESSIONE */
